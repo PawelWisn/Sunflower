77dc77b99f0b1b7e59fdd02b5a0b7654327293c0
Pawel Wisniewski <wispawelwis38@gmail.com>
2023-07-05 19:35:38 +0200

    add product model

--- sunflower/core/admin.py
+++ sunflower/core/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from .models import Product
 
 
 admin.site.unregister(Group)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "size", "quantity", "order")
+    list_filter = ("size",)
+    search_fields = ("name",)
--- sunflower/core/models.py
+++ sunflower/core/models.py
@@ -1,3 +1,26 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+from .constants import SMALL_SIZE, MEDIUM_SIZE, LARGE_SIZE
 
-# Create your models here.
+
+class Product(models.Model):
+    """Represents sellable item"""
+
+    sizes = (
+        (SMALL_SIZE, _("Small")),
+        (MEDIUM_SIZE, _("Medium")),
+        (LARGE_SIZE, _("Large")),
+    )
+
+    name = models.CharField(_("Name"), max_length=255)
+    quantity = models.PositiveIntegerField(_("Quantity"))
+    size = models.CharField(_("Size"), choices=sizes, max_length=3, db_index=True)
+    order = models.PositiveIntegerField(_("Order"), default=1, db_index=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.get_size_display()}"
+
+    class Meta:
+        ordering = ("order", "name")
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
