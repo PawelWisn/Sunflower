e3457feb1a70c3de4623fc4207c8dd0e099b9ded
Pawel Wisniewski <wispawelwis38@gmail.com>
2023-10-19 18:39:00 +0200

    display list of products on homepage

--- sunflower_fe/src/index.tsx
+++ sunflower_fe/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import router from "./routes/Router";
 import {RouterProvider} from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
