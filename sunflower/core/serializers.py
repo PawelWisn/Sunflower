45e7a1951fe1798cee961dd7eed01eb8dd980aed
Pawel Wisniewski <wispawelwis38@gmail.com>
2023-10-22 18:39:53 +0200

    add picture url to product list serializer

--- sunflower/core/serializers.py
+++ sunflower/core/serializers.py
@@ -11,6 +11,7 @@ class ProductListSerializer(ModelSerializer):
             "id",
             "name",
             "quantity",
+            "picture",
             "size",
             "order",
         )
